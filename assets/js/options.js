5d8b394642b9edfeadb29e98d0f6ecad1c8371ab
Ángel Cano Idáñez <nikeindex@gmail.com>
2023-02-04 21:39:53 +0100

    Master

---
+++ assets/js/options.js
@@ -0,0 +1,119 @@
+chrome.storage.sync.get(['myUrl'], checkSync);
+document.getElementById("save").addEventListener("click", saveUrl);
+document.getElementById('try').addEventListener('click', tryUrl);
+document.getElementById('delete').addEventListener('click', resetUrl);
+document.getElementById('about').addEventListener('click', about);
+document.getElementById('make').addEventListener('click', make);
+document.getElementById('ifttt').addEventListener('click', ifttt);
+document.getElementById('youtube').addEventListener('click', youtube);
+document.getElementById('help').addEventListener('click', help);
+
+function saveUrl(event) {
+  event.preventDefault();
+  var url = document.getElementById("url").value;
+  if (!url) {
+    alert('Please enter a URL');
+    return;
+  }
+  if (!url.match(/^(http|https):\/\/[a-zA-Z0-9-_.]+\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)) {
+    alert('Please enter a valid URL');
+    return;
+  }
+  chrome.storage.sync.set({ 'myUrl': url }, function () {
+    alert('URL saved successfully');
+    console.log("URL guardada en chrome.storage: " + url);
+  });
+}
+function tryUrl(event) {
+  event.preventDefault();
+  chrome.storage.sync.get(['myUrl'], function (result) {
+    var fieldUrl = document.getElementById("url").value;
+    if (!result.myUrl || !fieldUrl) {
+      alert('Please set a URL first and press save button');
+      return;
+    }
+    var url = result.myUrl;
+    fetch(url,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          title: 'test title',
+          description: 'test description',
+          url: 'https://api.publicapis.org/'
+        })
+      })
+      .then(res => {
+        if (res.ok) {
+          tryMessage(res)
+          console.log(res.status);
+          return res.json();
+        } else {
+          tryMessage(res);
+          throw new Error(res.statusText);
+        }
+      })
+
+      .catch(error => alert(`Error: ${error}`));
+  });
+}
+
+function resetUrl(event) {
+  event.preventDefault();
+  document.getElementById("url").value = 'https://api.publicapis.org/health';
+  chrome.storage.sync.set({ 'myUrl': 'https://api.publicapis.org/health' }, function () {
+    alert('URL reset successfully! Don\'t forget to store a new one!');
+    console.log("URL reseteada en chrome.storage");
+  });
+}
+
+function checkSync(result) {
+  var url = result.myUrl;
+  if (url) {
+    document.getElementById("url").value = url;
+  }
+}
+
+function tryMessage(res) {
+  var statusMsg = document.getElementById('status');
+  statusMsg.style.display = 'block';
+  if (res.ok) {
+    statusMsg.style.backgroundColor = 'green';
+    statusMsg.innerHTML = 'Request successful!';
+  } else {
+    statusMsg.style.backgroundColor = 'red';
+    statusMsg.innerHTML = 'Request failed!';
+  }
+  setTimeout(function () {
+    document.getElementById('status').style.display = 'none';
+  }, 5000);
+
+}
+
+function about() {
+  chrome.tabs.create({ url: "https://twitter.com/equisele" });
+}
+
+function make() {
+  chrome.tabs.create({ url: "https://www.make.com/en/register?pc=equisele" });
+}
+
+function ifttt() {
+  chrome.tabs.create({ url: "https://ifttt.com/join?referral_code=cLdgjkYTxFCULXIZMDLZDs616s6hudXA" });
+}
+
+function youtube() {
+  chrome.tabs.create({ url: "https://youtube.com/" });
+}
+
+function help() {
+  var help = document.getElementById('helpBox');
+  if (help.style.display === 'block') {
+    help.style.display = 'none';
+  } else {
+    help.style.display = 'block';
+  }
+}
---
+++ background.js
@@ -0,0 +1,150 @@
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.get("myUrl", function (result) {
+    if (!result.myUrl) {
+      chrome.storage.sync.set({ "myUrl": "https://api.publicapis.org/health" }, function () {
+        console.log("Default URL set in chrome.storage");
+      });
+    }
+  });
+
+  chrome.contextMenus.create({
+    "id": "browser2Webhook",
+    "title": "Browser 2 Webhook",
+    contexts: ["page", "selection"],
+  });
+
+
+  chrome.contextMenus.create({
+    "id": "sendText2webhook",
+    "parentId": "browser2Webhook",
+    "title": "Send Text to Webhook",
+    "contexts": ["selection"],
+    onclick: sendTextToWebhook
+  });
+
+  chrome.contextMenus.create({
+    id: "sendUrl2Webhook",
+    "parentId": "browser2Webhook",
+    title: "Send Url to Webhook",
+    contexts: ["page"],
+    onclick: sendUrlToWebhook
+  });
+});
+
+chrome.runtime.setUninstallURL("https://example.com/uninstall", function () {
+  chrome.storage.sync.remove("myUrl", function () {
+    console.log("myUrl deleted from chrome.storage");
+  });
+});
+
+function sendTextToWebhook(info, tab) {
+  chrome.storage.sync.get("myUrl", function (data) {
+    let url = data.myUrl;
+    chrome.tabs.executeScript({
+      code: `(function() {
+        return {
+          description: document.querySelector("meta[name='description']").getAttribute("content")
+        };
+      })();`
+    }, function (results) {
+      if (!results || !results[0]) {
+        var description = 'No description';
+      } else {
+        var description = results[0].description;
+      }
+      if (!tab.title || tab.title === '') {
+        var title = 'No title';
+      } else {
+        var title = tab.title;
+      }
+      if (!info.selectionText || info.selectionText === '') {
+        var textSelection = 'No text selected';
+      } else {
+        var textSelection = info.selectionText;
+      }
+      if (!url.match(/^(http|https):\/\/[a-zA-Z0-9-_.]+\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)) {
+        alert('This tab url is not valid! Please, use on a valid tab.');
+        return;
+      }
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title: title,
+          description: description,
+          url: tab.url,
+          text: textSelection
+        })
+      })
+        .then(response => {
+          if (response.ok) {
+            alert('Done! Request successful!');
+            console.log(response.status);
+            console.log("Done! Request successful!");
+          } else {
+            throw new Error('Request failed!');
+          }
+        })
+        .catch(error => {
+          alert('Request failed!');
+          console.log(response.status);
+          console.log("Request failed!");
+        });
+    });
+  });
+}
+
+function sendUrlToWebhook(info, tab) {
+  chrome.storage.sync.get("myUrl", function (data) {
+    let url = data.myUrl;
+    chrome.tabs.executeScript({
+      code: `(function() {
+        return {
+          description: document.querySelector("meta[name='description']").getAttribute("content")
+        };
+      })();`
+    }, function (results) {
+      if (!results || !results[0]) {
+        var description = 'No description';
+      } else {
+        var description = results[0].description;
+      }
+      if (!tab.title || tab.title === '') {
+        var title = 'No title';
+      } else {
+        var title = tab.title;
+      }
+      if (!url.match(/^(http|https):\/\/[a-zA-Z0-9-_.]+\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)) {
+        alert('This tab url is not valid! Please, use on a valid tab.');
+        return;
+      }
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title: title,
+          description: description,
+          url: tab.url
+        })
+      })
+        .then(response => {
+          if (response.ok) {
+            alert('Done! Request successful!');
+            console.log(response.status);
+            console.log("Done! Request successful!");
+          } else {
+            throw new Error('Request failed!');
+          }
+        })
+        .catch(error => {
+          alert('Request failed!');
+          console.log(response.status);
+          console.log("Request failed!");
+        });
+    });
+  });
+}
